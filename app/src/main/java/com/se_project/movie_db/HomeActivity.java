a81af18a9239bac41d36de60a9db61120e1c0d38
SandhuJai <sandhujaikirat98@gmail.com>
2018-04-21 03:07:44 +0530

    List Activity Functional

--- app/src/main/java/com/se_project/movie_db/HomeActivity.java
+++ app/src/main/java/com/se_project/movie_db/HomeActivity.java
@@ -1,11 +1,14 @@
 package com.se_project.movie_db;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import org.json.JSONArray;
@@ -20,21 +23,25 @@ public class HomeActivity extends AppCompatActivity {
     private RecyclerView.Adapter coming_soon_adapter;
     private RecyclerView.LayoutManager coming_soon_layout_manager;
     private List<Movie> comingSoonMovies;
+    private Button coming_soon_movies_see_all_button;
 
     private RecyclerView in_theatres_recyclerView;
     private RecyclerView.Adapter in_theatres_adapter;
     private RecyclerView.LayoutManager in_theatres_layout_manager;
     private List<Movie> inTheatersMovies;
+    private Button in_theatre_movies_see_all_button;
 
     private RecyclerView popular_movies_recyclerView;
     private RecyclerView.Adapter popular_movies_adapter;
     private RecyclerView.LayoutManager popular_movies_layout_manager;
     private List<Movie> popularMovies;
+    private Button popular_movies_see_all_button;
 
     private RecyclerView top_rated_movies_recyclerView;
     private RecyclerView.Adapter top_rated_movies_adapter;
     private RecyclerView.LayoutManager top_rated_movies_layout_manager;
     private List<Movie> top_rated_Movies;
+    private Button top_rated_movies_see_all_button;
 
     private boolean comingSoonRunning, inTheaterRunning, popularMoviesRunning, topRatedRunning;
 
@@ -86,6 +93,7 @@ public class HomeActivity extends AppCompatActivity {
         coming_soon_layout_manager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         coming_soon_recyclerView.setLayoutManager(coming_soon_layout_manager);
         comingSoonRunning = true;
+        coming_soon_movies_see_all_button = findViewById(R.id.coming_soon_see_all);
 
         new GetComingSoon().execute();
 
@@ -95,6 +103,7 @@ public class HomeActivity extends AppCompatActivity {
         in_theatres_layout_manager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         in_theatres_recyclerView.setLayoutManager(in_theatres_layout_manager);
         inTheaterRunning = true;
+        in_theatre_movies_see_all_button = findViewById(R.id.in_theatres_see_all);
 
         new GetInTheater().execute();
 
@@ -104,6 +113,7 @@ public class HomeActivity extends AppCompatActivity {
         popular_movies_layout_manager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         popular_movies_recyclerView.setLayoutManager(popular_movies_layout_manager);
         popularMoviesRunning = true;
+        popular_movies_see_all_button = findViewById(R.id.popular_movies_see_all);
 
         new GetPopularMovies().execute();
 
@@ -113,8 +123,50 @@ public class HomeActivity extends AppCompatActivity {
         top_rated_movies_layout_manager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         top_rated_movies_recyclerView.setLayoutManager(top_rated_movies_layout_manager);
         topRatedRunning = true;
+        top_rated_movies_see_all_button = findViewById(R.id.top_rated_see_all);
 
         new GetTopRated().execute();
+
+        setSeeAllButtons();
+    }
+
+    // Add onClickListeners to all See All Buttons so that clicking them sends Intent to ListActivity
+    private void setSeeAllButtons() {
+        coming_soon_movies_see_all_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ListActivity.titleText = "Coming Soon";
+                ListActivity.pageCnt = 0;
+                startActivity(new Intent(HomeActivity.this, ListActivity.class));
+            }
+        });
+
+        in_theatre_movies_see_all_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ListActivity.titleText = "In Theatres";
+                ListActivity.pageCnt = 0;
+                startActivity(new Intent(HomeActivity.this, ListActivity.class));
+            }
+        });
+
+        popular_movies_see_all_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ListActivity.titleText = "Most Popular Movies";
+                ListActivity.pageCnt = 0;
+                startActivity(new Intent(HomeActivity.this, ListActivity.class));
+            }
+        });
+
+        top_rated_movies_see_all_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ListActivity.titleText = "Top Rated Movies";
+                ListActivity.pageCnt = 0;
+                startActivity(new Intent(HomeActivity.this, ListActivity.class));
+            }
+        });
     }
 
     private class GetComingSoon extends AsyncTask<Void, Void, Void> {
---
+++ app/src/main/java/com/se_project/movie_db/ListActivity.java
@@ -0,0 +1,151 @@
+package com.se_project.movie_db;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListActivity extends AppCompatActivity {
+    // TODO: Change List View's Name according to the activity
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ProgressDialog progressDialog;
+    private Button seeMore;
+
+    private TextView titleOfPage;   // Title of Page comes here
+
+    public static String titleText;
+    private List<Movie> movies;
+
+    public static int pageCnt;
+
+    private StringBuilder query;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        seeMore = findViewById(R.id.see_more_button);
+        seeMore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new GetList().execute();
+            }
+        });
+
+        movies = new ArrayList<>();
+
+        query = new StringBuilder("https://api.themoviedb.org/3/movie/");
+
+        if(titleText.equalsIgnoreCase("Coming Soon")) {
+            query.append("upcoming");
+        }else if(titleText.equalsIgnoreCase("In Theatres")) {
+            query.append("now_playing");
+        }else if(titleText.equalsIgnoreCase("Most Popular Movies")) {
+            query.append("popular");
+        }else if(titleText.equalsIgnoreCase("Top Rated Movies")) {
+            query.append("top_rated");
+        }
+
+        query.append("?api_key=5da5237ab7ea9020ac16bcd6e8ffe0ab&language=en-US");
+
+        titleOfPage = findViewById(R.id.listViewTitle);
+        titleOfPage.setText(titleText);
+
+        recyclerView = findViewById(R.id.generic_list_recycler_view);
+        recyclerView.setHasFixedSize(false);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        new GetList().execute();
+    }
+
+    private class GetList extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            pageCnt++;
+            query.append("&page=" + pageCnt + "&region=US");
+            progressDialog = new ProgressDialog(ListActivity.this);
+            progressDialog.setMessage("Loading Results...");
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            if(pageCnt == 1) {
+                adapter = new CardAdapter(movies);
+                recyclerView.setAdapter(adapter);
+            }else {
+                adapter.notifyDataSetChanged();
+            }
+
+            if(progressDialog.isShowing()) {
+                progressDialog.dismiss();
+            }
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            HttpHandler sh = new HttpHandler();
+
+            String jsonStr = sh.makeServiceCall(query.toString());
+
+            if(jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+
+                    // Getting Name
+                    JSONArray results = jsonObj.getJSONArray("results");
+
+                    for(int i = 0; i < results.length(); i++) {
+                        JSONObject temp = results.getJSONObject(i);
+
+                        String title = temp.getString("original_title");
+                        String url = temp.getString("poster_path");
+
+                        movies.add(new Movie(title, "https://image.tmdb.org/t/p/w500" + url));
+                    }
+                } catch (JSONException e) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(), "JSON parsing error", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+            }else {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "Couldn't get json from server.", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+
+            return null;
+        }
+    }
+
+}
