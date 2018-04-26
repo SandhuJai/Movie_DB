0eff8c553230cc78d797a384ae058d31d021fa13
SandhuJai <sandhujaikirat98@gmail.com>
2018-04-27 01:13:07 +0530

    LogIn/SignUp initiated

---
+++ app/src/main/java/com/se_project/movie_db/SignUpActivity.java
@@ -0,0 +1,13 @@
+package com.se_project.movie_db;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
