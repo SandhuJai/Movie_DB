86a09000933e1ff9a4d5743c18609e790cc728f6
SandhuJai <sandhujaikirat98@gmail.com>
2018-04-27 02:25:59 +0530

    LogIn/SignUp initiated

--- app/src/main/java/com/se_project/movie_db/WelcomeActivity.java
+++ app/src/main/java/com/se_project/movie_db/WelcomeActivity.java
@@ -1,13 +1,49 @@
 package com.se_project.movie_db;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class WelcomeActivity extends AppCompatActivity {
+    private Button signInWithEmailBtn;
+    private Button signUpBtnBtn;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser != null) {
+            startActivity(new Intent(WelcomeActivity.this, HomeActivity.class));
+            finish();
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        signInWithEmailBtn = findViewById(R.id.sign_in_with_email);
+        signUpBtnBtn = findViewById(R.id.create_an_account);
+
+        signUpBtnBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(WelcomeActivity.this, SignUpActivity.class));
+            }
+        });
+
+        signInWithEmailBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(WelcomeActivity.this, LogInActivity.class));
+            }
+        });
     }
 }
